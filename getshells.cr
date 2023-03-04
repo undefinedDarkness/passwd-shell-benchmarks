f2acbfc03570c4108b7fcb82260d670c4d432398
izder456 <izder456@srcerizder.top>
2023-03-04 17:27:54 -0600

    re-write ruby and crystal, incorporate hash maps.

--- getshells.cr
+++ getshells.cr
@@ -1,16 +1,21 @@
 #!/usr/bin/env crystal
 
-shellcnt = {} of String => Int32
+# Define a Hash to store the count of each login shell
+shellcnt = Hash(String, Int32).new(0)
 
-if File.exists?("passwd")
-   File.each_line("passwd") do |line|
-      gecos = line.chomp.split(':')
-      shell = gecos.last
-      shellcnt[shell] ||= 0
-      shellcnt[shell] += 1
-   end
+# Open the passwd file and iterate over each line
+File.open("passwd") do |file|
+  file.each_line do |line|
+    # Split the line into fields using colon as the delimiter
+    fields = line.chomp.split(':')
+    # Extract the login shell from the last field
+    shell = fields[-1]
+    # Increment the count of this login shell in the Hash
+    shellcnt[shell] += 1
+  end
+end
 
-   shellcnt.each do |key, value|
-      printf("%-20s%-8s%-d\n", key, ":", value)
-   end
+# Iterate over the Hash and print the counts for each login shell
+shellcnt.each do |key, value|
+  printf("%-20s%-8s%-d\n", key, ":", value)
 end
