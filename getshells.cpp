8c97dede5f404b2df0fd51c5553e27ec73873baa
izder456 <izder456@srcerizder.top>
2023-03-08 16:35:30 -0600

    add C++

---
+++ getshells.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    // Open the password file for reading.
+    ifstream passwd_file("passwd");
+
+    // Define a hash table to store the number of instances of each shell.
+    unordered_map<string, int> shell_counts;
+
+    // Read the file line by line and count the instances of each shell.
+    string line;
+    while (getline(passwd_file, line)) {
+        // Find the last colon-separated field (the login shell).
+        size_t last_colon = line.rfind(':');
+        string shell = line.substr(last_colon + 1);
+
+        // Increment the count of this shell.
+        ++shell_counts[shell];
+    }
+
+    // Print out the counts for each shell in three columns.
+    int column_width = 25;
+    for (const auto& kv : shell_counts) {
+        cout << kv.first << string(column_width - kv.first.length(), ' ')
+             << ":    " << kv.second << endl;
+    }
+
+    return 0;
+}
