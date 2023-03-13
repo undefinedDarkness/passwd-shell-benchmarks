8063508d6275101b094b0582364cb9d972e9a7d5
CordlessCoder <>
2023-03-13 13:27:42 +0000

    added multithreaded Rust version, updated main Rust version

--- getshells_rust/src/main.rs
+++ getshells_rust/src/main.rs
@@ -1,3 +1,7 @@
+#![feature(maybe_uninit_slice)]
+#![feature(new_uninit)]
+#![feature(read_buf)]
+#![feature(hash_raw_entry)]
 use std::{
     fmt::Display,
     fs::File,
@@ -13,49 +17,52 @@ fn main() {
     const FILE: &str = "passwd";
     // Read buffer size is also a fragile optimization, mess around with capcacities 64 Kib -
     // 1024 Kib to find the best result
-    const CHUNK: usize = 128 * 1024;
+    const CHUNK: usize = 64 * 1024;
 
-    let file = BufReader::with_capacity(CHUNK, File::open(FILE).expect("failed to read {FILE}"));
+    let file = File::open(FILE).expect("failed to read {FILE}");
+    let file = BufReader::with_capacity(CHUNK, file);
+    //
     // Use aHash on AVX enabled platforms, should be faster
     #[cfg(target_feature = "avx")]
-    let mut hs = AHashMap::with_capacity(32); // Initial capacity 32 performed the best at the time
-                                              // of testing, probably a fragile optimization
+    let mut hs = AHashMap::with_capacity(512); // Initial capacity 32 performed the best at the time
+                                               // of testing, probably a fragile optimization
     #[cfg(not(target_feature = "avx"))]
-    let mut hs = HashMap::with_capacity(32);
-    file.split(b'\n')
-        .try_for_each(|line| -> io::Result<()> {
-            let mut line = line?;
-            let colon_idx = line
-                .iter()
-                .enumerate()
-                .rev()
-                // This mess finds the first `:` from the end of the line
-                .find(|(_, &b)| b == b':')
-                .map(|(idx, _)| idx)
-                .unwrap_or_default();
-            line.drain(0..=colon_idx); // Remove all elements up to and including `:`
-
-            // Only needed if we assume the file can be CRLF encoded
-            // (it can't because that'd be weird)
-            //
-            // if line.last() == Some(&b'\r') {
-            //     line.pop();
-            // }
-            hs.entry(line).and_modify(|x| *x += 1u32).or_insert(1);
-            Ok(())
-        })
-        .unwrap();
-
+    let mut hs = HashMap::with_capacity(512);
     let mut stdout = stdout().lock();
+    let _ = file.split(b'\n').try_for_each(|line| -> io::Result<()> {
+        let line = line?;
+        let colon_idx = line
+            .iter()
+            .enumerate()
+            .rev()
+            // This mess finds the first `:` from the end of the line
+            .find(|(_, &b)| b == b':')
+            .map(|(idx, _)| idx)
+            .unwrap_or_default();
+
+        // Only needed if we assume the file can be CRLF encoded
+        // (it can't because that'd be weird)
+        //
+        // if line.last() == Some(&b'\r') {
+        //     line.pop();
+        // }
+        let shell = &line[colon_idx + 1..];
+        hs.raw_entry_mut()
+            .from_key(shell)
+            // .from_key(&line[colon_idx + 1..])
+            .and_modify(|_, v| *v += 1)
+            .or_insert_with(|| (shell.to_vec(), 1));
+        Ok(())
+    });
 
     hs.into_iter().for_each(|(shell, count)| {
-        let _ = stdout.write_fmt(format_args!("{}: {count}", UnsafeBytes(shell)));
+        let _ = stdout.write_fmt(format_args!("{}: {count}\n", UnsafeBytes(&shell)));
     });
 }
 
 #[repr(transparent)]
-struct UnsafeBytes(Vec<u8>);
-impl Display for UnsafeBytes {
+struct UnsafeBytes<'a>(&'a [u8]);
+impl<'a> Display for UnsafeBytes<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(unsafe { std::str::from_utf8_unchecked(&self.0) })
     }
