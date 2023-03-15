793c45f049593cebe03b0b5d5d6edcb6abd4f18e
izder456 <izder456@srcerizder.top>
2023-03-15 15:15:32 -0500

    fix indentation and clarify comments

--- getshells.cr
+++ getshells.cr
@@ -5,16 +5,16 @@ shellcnt = Hash(String, Int32).new(0)
 
 # Open passwd file and read line by line
 File.open("passwd") do |file|
-  file.each_line do |line|
-    # Extract the shell from the last field of the line
-    shell = line.split(':').last.chomp
+    file.each_line do |line|
+        # Extract the shell from the last field of the line
+        shell = line.split(':').last.chomp
 
-    # Increment the count for the given shell in the hash
-    shellcnt[shell] += 1
-  end
+        # Increment the count for the given shell in the hash
+        shellcnt[shell] += 1
+    end
 end
 
 # Print the shell count in formatted output
 shellcnt.each do |shell, count|
-  printf("%-20s%-8s%-d\n", shell, ":", count)
+    printf("%-20s%-8s%-d\n", shell, ":", count)
 end
--- getshells_m.cr
+++ getshells_m.cr
@@ -1,7 +1,7 @@
 #!/usr/bin/env crystal
 
 
-# Define the number of chunks to split the file into
+# Define the size of chunks to split the file into
 CHUNK_SIZE = 1000
 
 # Create an empty hash to store shell and its count
@@ -11,24 +11,24 @@ shellcnt = Hash(String, Int32).new(0)
 File.open("passwd") do |file|
     chunks = file.each_line.each_slice(CHUNK_SIZE).to_a
 
-  fibers = chunks.map do |chunk|
-    # Process each line in the chunk and update the shell count
-    spawn do
-      chunk.each do |line|
-        # Extract the shell from the last field of the line
-        shell = line.split(':').last.chomp
+    fibers = chunks.map do |chunk|
+        # Process each line in the chunk and update the shell count
+        spawn do
+            chunk.each do |line|
+                # Extract the shell from the last field of the line
+                shell = line.split(':').last.chomp
 
-        # Increment the count for the given shell in the hash
-        shellcnt[shell] += 1
-      end
+                # Increment the count for the given shell in the hash
+                shellcnt[shell] += 1
+            end
+        end
     end
-  end
 
-  # Wait for all futures to complete
-  Fiber.yield 
+    # Wait for all fibers to complete
+    Fiber.yield 
 end
 
 # Print the shell count in formatted output
 shellcnt.each do |shell, count|
-  printf("%-20s%-8s%-d\n", shell, ":", count)
+    printf("%-20s%-8s%-d\n", shell, ":", count)
 end
