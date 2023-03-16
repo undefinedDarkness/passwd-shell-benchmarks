49bd0d901c93a62d08964fccc179a44113001763
undefinedDarkness <davidnoronha@outlook.in>
2023-03-16 14:54:02 +0530

    slightly faster

--- getshells.c
+++ getshells.c
@@ -1,51 +1,54 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-  /* int fd = open("passwd", O_RDONLY);
-  struct stat s;
-  fstat(fd, &s);
-  size_t fs = s.st_size;
-  char *contents = mmap(0, fs, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-  */
+static __always_inline char* itoa(unsigned short value, char* result) {
+	int i = 0;
+	while (value) {
+		result[9 - i++] = (value % 10) + '0';
+		value /= 10;
+	}
+	result[10] = '\0';
+	return result+(10-i);
+}
 
+int main() {
   struct shell {
-    char name[32];
-    int count;
+    char name[20];
+	int count; 
   };
 
   void *heap =
-      calloc(1, 512 + sizeof(struct shell) *
-                          200); // combine allocations for both into one fat one
-  struct shell *buf = heap; // calloc(200, sizeof(buf)); // lil extra space cuz
+      calloc(1,128*(sizeof(struct shell)+1)); // combine allocations for both into one fat one
+  struct shell *restrict buf = heap; // calloc(200, sizeof(buf)); // lil extra space cuz
                             // im allocating anyway avoids memory errors
-  char *buffer = heap + (sizeof(struct shell) * 200);
+  char *buffer = heap + (sizeof(struct shell) * 128);
 
   FILE *fp = fopen("passwd", "r");
-  size_t bufSize = 512;
+  size_t bufSize = 128;
   size_t lineSize;
 
   size_t ts = 0;
 
-  while ((lineSize = getline(&buffer, &bufSize, fp)) != -1) {
-    char *colon = buffer + lineSize - 6; // smallest is 7
-    while (*colon != ':')
-      colon--;
-    colon++;
-    size_t length = buffer + lineSize - colon - 1;
-    size_t id = (buffer[lineSize - 4] ^ length + buffer[lineSize - 5]) % 50; // hash(colon);
+  while ((lineSize = getline(&buffer, &bufSize, fp)) != (size_t)-1) {
+	const char* const colon = memrchr(buffer, ':', lineSize-6) + 1;			// TODO: Replace memrchr with precalculated positions once I figure out the math
+    const size_t length = buffer + lineSize - colon - 1;
+	const size_t id = (buffer[lineSize - 4] ^ length + buffer[lineSize - 5]) % 50; // hash(colon);
     buf[id].count++;
     if (0 == *buf[id].name) {
       memcpy(buf[id].name, colon, length);
-      buf[id].name[length] = '\0';
+      buf[id].name[length] = '\t';
+	  buf[id].name[length+1] = '\0';
       ts++;
     }
   }
 
   for (int i = 0; i < 100; i++) {
-    if (buf[i].count > 0) {
-      printf("%s\t%d\n", buf[i].name, buf[i].count);
+    struct shell s = buf[i];
+	  if (s.count > 0) {
+		  fputs(s.name, stdout);
+		  puts(itoa(s.count, buf));
       if (!--ts)
         break;
     }
@@ -53,7 +56,4 @@ int main() {
 
   free(heap);
   fclose(fp);
-  // free(buf);
-  // munmap(contents, fs);
-  // close(fd);
 }
